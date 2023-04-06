b8a9f0ddddb20d41688bda76457707f854b29707
codenoobies <kevindubidaam@gmail.com>
2023-04-06 11:16:21 +0700

    Changed Bottom Bar background Color

--- src/components/Layout/TheBottomBar.vue
+++ src/components/Layout/TheBottomBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex fixed items-center justify-between bottom-0 left-0 h-16 px-10 py-3 bg-slate-500 w-full bottom-navbar md:hidden">
+    <nav class="flex fixed items-center justify-between bottom-0 left-0 h-16 px-10 py-3 bg-primary w-full bottom-navbar md:hidden">
         <div class="relative w-[50px] h-[50px]">
             <router-link to="/">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 500" width="35px" height="35px"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
