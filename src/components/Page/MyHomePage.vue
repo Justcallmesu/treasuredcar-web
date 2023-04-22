7eee80a73e77f85f83b0b10c383d32fe180e1d9e
codenoobies <kevindubidaam@gmail.com>
2023-04-22 17:33:51 +0700

    Finished Hero galleries img

--- src/components/Page/MyHomePage.vue
+++ src/components/Page/MyHomePage.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="py-10 px-5 pt-[8rem] bg-[url('/public/img/Home/heroimg.jpg')]  bg-gray-600 min-h-screen">
+    <div class="py-10 pb-20 px-5 pt-[8rem] relative overflow-x-hidden text-white bg-[url('/public/img/Home/heroimg.jpg')] bg-cover min-h-screen flex flex-col bg-no-repeat md:block">
         <h1 class="before:inline-block before:absolute before:-top-3 
         before:h-1 before:w-16 before:content-[''] before:bg-black 
         font-semibold tracking-[0.2rem] text-3xl font-bebasNeue relative">We Provide <span class="text-primary">Second <br/> Cars</span> For Purchase</h1>
         <h3 class="mt-5 mb-20">Purchase directly from local suppliers. no commission, no mark-ups</h3>
         <SearchCard></SearchCard>
-
+        <HeroGalleries/>
     </div>
 </template>
 
 <script>
 import SearchCard from '../HomePage/SearchCard.vue';
+import HeroGalleries from '../HomePage/HeroGalleries.vue';
 
 export default{
     components:{
-        SearchCard
+        SearchCard,
+        HeroGalleries
     }
 }
 
